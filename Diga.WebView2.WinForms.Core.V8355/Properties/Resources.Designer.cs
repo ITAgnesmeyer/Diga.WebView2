6008e4a75bcdde6d06a3886c739ab4afcc31b36a
DIGA <guido.agnesmeyer@itagnesmeyer.de>
2020-04-27 16:30:01 +0200

    Projekt umbenannt.

--- Diga.WebView2.WinForms.Core.V8355/Properties/Resources.Designer.cs
+++ Diga.WebView2.WinForms.Core.V8355/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Diga.WebView2.WinForms.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Diga.WebView2.WinFomrs.Core.V8355.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Diga.WebView2.WinForms.Core.V8355.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
