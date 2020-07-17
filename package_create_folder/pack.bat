fa175a95e882f6778a31d5b5f6362d8b4bb2437a
DIGA <guido.agnesmeyer@itagnesmeyer.de>
2020-07-17 11:06:00 +0200

    Package update

--- package_create_folder/pack.bat
+++ package_create_folder/pack.bat
@@ -1,9 +1,21 @@
-nuget.exe pack Diga.WebView2.Interop\V8335\Diga.WebView2.Interop.nuspec
-nuget.exe pack Diga.WebView2.Wrapper\V8335\Diga.WebView2.Wrapper.nuspec
-nuget.exe pack Diga.WebView2.WinForms\V8335\Diga.WebView2.WinForms.nuspec
-pause
-nuget.exe pack Diga.WebView2.Interop\V9430\Diga.WebView2.Interop.nuspec
-nuget.exe pack Diga.WebView2.Wrapper\V9430\Diga.WebView2.Wrapper.nuspec
-nuget.exe pack Diga.WebView2.WinForms\V9430\Diga.WebView2.WinForms.nuspec
-pause
+rem nuget.exe pack Diga.WebView2.Interop\V8335\Diga.WebView2.Interop.nuspec
+rem nuget.exe pack Diga.WebView2.Wrapper\V8335\Diga.WebView2.Wrapper.nuspec
+rem nuget.exe pack Diga.WebView2.WinForms\V8335\Diga.WebView2.WinForms.nuspec
+rem pause
+rem nuget.exe pack Diga.WebView2.Interop\V9430\Diga.WebView2.Interop.nuspec
+rem nuget.exe pack Diga.WebView2.Wrapper\V9430\Diga.WebView2.Wrapper.nuspec
+rem nuget.exe pack Diga.WebView2.WinForms\V9430\Diga.WebView2.WinForms.nuspec
+rem pause
 
+rem nuget.exe pack Diga.WebView2.Interop\V9488\Diga.WebView2.Interop.nuspec
+rem nuget.exe pack Diga.WebView2.Wrapper\V9488\Diga.WebView2.Wrapper.nuspec
+rem nuget.exe pack Diga.WebView2.WinForms\V9488\Diga.WebView2.WinForms.nuspec
+rem pause
+rem nuget.exe pack Diga.WebView2.Interop\V9515\Diga.WebView2.Interop.nuspec
+rem nuget.exe pack Diga.WebView2.Wrapper\V9515\Diga.WebView2.Wrapper.nuspec
+rem nuget.exe pack Diga.WebView2.WinForms\V9515\Diga.WebView2.WinForms.nuspec
+rem pause
+nuget.exe pack Diga.WebView2.Interop\V9538\Diga.WebView2.Interop.nuspec
+nuget.exe pack Diga.WebView2.Wrapper\V9538\Diga.WebView2.Wrapper.nuspec
+nuget.exe pack Diga.WebView2.WinForms\V9538\Diga.WebView2.WinForms.nuspec
+pause
